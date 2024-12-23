068cbcd2c7756d89d32ee796360f74bef0748fa6
Anton Lyhin <dotnet@lyhin.com>
2024-12-23 12:32:03 +0300

    Add yml;

--- Program.cs
+++ Program.cs
@@ -16,11 +16,11 @@ namespace AGSR.TestTask
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseAuthorization();
 
