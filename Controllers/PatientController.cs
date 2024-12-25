dd9285e10980237843d4071bc4dfc06625c5ad39
Anton Lyhin <dotnet@lyhin.com>
2024-12-25 16:31:43 +0300

    3. Birthday search with exact datetime;

--- Controllers/PatientController.cs
+++ Controllers/PatientController.cs
@@ -1,4 +1,7 @@
-﻿using AGSR.TestTask.Contexts;
+﻿using System.Globalization;
+using System.Linq.Expressions;
+using AGSR.TestTask.Contexts;
+using AGSR.TestTask.DynamicQueryExpressions;
 using AGSR.TestTask.Models;
 using AGSR.TestTask.ViewModels;
 using AutoMapper;
@@ -30,6 +33,85 @@ public class PatientController : ControllerBase
         var patientVMs = _mapper.Map<List<PatientWithOptionalNameViewModel>>(patients);
 
         return Ok(patientVMs);
+    }
+
+
+    /// <summary>
+    /// In order to avoid unintentional overlapping of ranges, requests can increase the specificity of their request. For example, the test for sa2013-01-14T00:00:00 is a much clearer test.
+    /// </summary>
+    [SwaggerOperation(Summary = "List all Patients, param example: 'eq2013-01-14T00:00:00Z'.")]
+    [HttpGet("birthDate")]
+    public async Task<ActionResult> GetPatientsByBirthDate([FromQuery] string birthDate = "eq2013-01-14T00:00:00Z")
+    {
+        var query = _agsrContext.Patients.AsQueryable();
+
+        if (birthDate == null)
+        {
+            var result = _mapper.Map<List<PatientWithOptionalNameViewModel>>(query);
+            return Ok(result);
+        }
+
+        try
+        {
+            var filterType = birthDate.Substring(0, 2).ToString().ToLower();
+            var datePart = birthDate.Substring(2).ToString();
+            var date = DateTimeOffset.Parse(datePart, CultureInfo.InvariantCulture);
+
+            var param = Expression.Parameter(typeof(PatientModel), "p");
+            var member = Expression.Property(param, "BirthDate");
+
+            switch (filterType)
+            {
+                case "eq":
+                    var expressionEQ = DynamicExpressions.CreateEQExpression(param, member, date); // eq2013-01-14T00:00:00Z
+                    query = query.Where(expressionEQ);
+                    break;
+                case "ne":
+                    var expressionNE = DynamicExpressions.CreateNEExpression(param, member, date); // ne2013-01-14T00:00:00Z
+                    query = query.Where(expressionNE);
+                    break;
+
+                case "lt":
+                    query = query.Where(x => x.BirthDate < date.UtcDateTime);
+                    break;
+                case "gt":
+                    query = query.Where(x => x.BirthDate > date.UtcDateTime);
+                    break;
+
+                case "ge":
+                    query = query.Where(x => x.BirthDate >= date.UtcDateTime);
+                    break;
+                case "le":
+                    query = query.Where(x => x.BirthDate <= date.UtcDateTime);
+                    break;
+
+                case "sa": // starts after
+                    var rightDate = date.UtcDateTime;
+                    query = query.Where(x => rightDate < x.BirthDate);
+                    break;
+                case "eb": // ends before
+                    var leftDate = date.UtcDateTime;
+                    query = query.Where(x => x.BirthDate < leftDate);
+                    break;
+                case "ap": // the resource value is approximately the same to the parameter value
+                    var leftDateAP = date.UtcDateTime;
+                    var rightDateAP = date.UtcDateTime;
+                    query = query.Where(x => leftDateAP <= x.BirthDate && x.BirthDate <= rightDateAP);
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(filterType);
+            }
+
+            var queryString = query.ToQueryString();
+            var queryList = query.ToList();
+
+            return Ok(_mapper.Map<List<PatientWithOptionalNameViewModel>>(queryList));
+        }
+        catch (Exception e)
+        { 
+            return BadRequest(e.Message);
+        }
     }
 
     [SwaggerOperation(Summary = "Get single Patient.")]
---
+++ DynamicQueryExpressions/DynamicExpressions.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq.Expressions;
+using AGSR.TestTask.Models;
+
+namespace AGSR.TestTask.DynamicQueryExpressions;
+
+public static class DynamicExpressions
+{
+    public static Expression<Func<PatientModel, bool>> CreateEQExpression(ParameterExpression param, MemberExpression member, DateTimeOffset value)
+    {
+        var expression = GetEQExpression(value, member);
+        return Expression.Lambda<Func<PatientModel, bool>>(expression, param);
+    }
+
+    public static Expression<Func<PatientModel, bool>> CreateNEExpression(ParameterExpression param, MemberExpression member, DateTimeOffset value)
+    {
+        var expression = Expression.Not(GetEQExpression(value, member));
+        return Expression.Lambda<Func<PatientModel, bool>>(expression, param);
+    }
+
+    private static Expression GetEQExpression(DateTimeOffset value, MemberExpression member)
+    {
+        var constantA = Expression.Constant(value.UtcDateTime.Date);
+        var expressionA = Expression.GreaterThanOrEqual(member, constantA);
+
+        var constantB = Expression.Constant(value.UtcDateTime.AddDays(1).Date);
+        var expressionB = Expression.LessThan(member, constantB);
+
+        return Expression.And(expressionA, expressionB);
+    }
+}
