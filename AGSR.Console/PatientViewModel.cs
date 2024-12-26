f7714ab57ba03d625133fb98a5554882efd79830
Anton Lyhin <dotnet@lyhin.com>
2024-12-26 17:20:59 +0300

    4. AGSR.Console add configuration and change port;

---
+++ AGSR.Console/PatientViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AGSR.TestTask.ViewModels;
+
+public class PatientViewModel
+{
+    public Guid Id { get; set; }
+    public string? Use { get; set; }
+    public string Family { get; set; }
+    public string[]? Given { get; set; }
+    public string? Gender { get; set; }
+    public DateTime BirthDate { get; set; }
+    public bool? Active { get; set; }
+}
--- AGSR.Console/Program.cs
+++ AGSR.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Http.Json;
 using AGSR.TestTask.ViewModels;
 using AutoFixture;
+using Microsoft.Extensions.Configuration;
 
 namespace AGSR.Console;
 
@@ -14,18 +15,26 @@ internal class Program
         "unknown"
     };
 
+    public class AppSettings
+    {
+        public string ApiUrl { get; set; }
+    }
+
     static async Task Main(string[] args)
     {
         var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("config.json", optional: false);
+                .AddJsonFile("appsettings.json", optional: false);
+
+        IConfiguration config = builder.Build();
+        var settings = config.Get<AppSettings>();
 
         var fixture = new Fixture();
         fixture.Customize<PatientViewModel>(x => x.With(prop => prop.Gender, () => GenderValues[new Random().Next() % 4]));
         var patientVMs = Enumerable.Range(1, 100).Select(x => fixture.Create<PatientViewModel>()).ToList();
 
         using var client = new HttpClient();
-        var response = await client.PostAsJsonAsync("http://localhost:5285/patient", patientVMs);
+        var response = await client.PostAsJsonAsync(settings.ApiUrl, patientVMs);
         if (response.IsSuccessStatusCode)
         {
             System.Console.WriteLine("OK");
